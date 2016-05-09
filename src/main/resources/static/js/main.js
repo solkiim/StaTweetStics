a5a845b6f24eaae4b6b72525eb7a6e52b70daaf1
Sol Kim <solkiim@users.noreply.github.com>
2016-05-09 01:38:27 -0400

    connected

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -271,7 +271,7 @@ $("#usernameEdit").click(function() {
                     usersToCompare.push(this.value);
                 }  
             });
-            //getCompareUsers();
+            getCompareUsers();
         }
     } else {
         editingUsername = true;
